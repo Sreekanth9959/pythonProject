7c381e0903f280470fd347c2d283c935af35b3fe
Sreekanth9959 <163122361+Sreekanth9959@users.noreply.github.com>
2024-03-12 15:51:26 +0530

    Add files via upload

---
+++ Armstrong number.py
@@ -0,0 +1,19 @@
+def a(number):
+    num_str = str(number)
+    num_digits = len(num_str)
+
+    armstrong_sum = 0
+
+    for digit in num_str:
+        armstrong_sum += int(digit) ** num_digits
+
+    if armstrong_sum == number:
+        return True
+    else:
+        return False
+
+number = int(input("Enter a number: "))
+if a(number):
+    print(number, "is an Armstrong number.")
+else:
+    print(number, "is not an Armstrong number.")
